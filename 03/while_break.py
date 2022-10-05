2c0d92ecae04f2a76f2a5a3612ad0c51066308db
Pavučina <pavucina.p@gmail.com>
2022-10-05 21:56:25 +0200

    Pyladies - lesson 3.

---
+++ 03/while_break.py
@@ -0,0 +1,7 @@
+from random import randrange
+
+while True:
+    if 13 == randrange(100):
+        print("Super, to by šlo!")
+        break                           # ukončí cyklus
+    print("Ne!")
---
+++ 03/zelva.py
@@ -0,0 +1,13 @@
+from turtle import forward, right, left
+from turtle import shape, exitonclick
+
+shape("turtle")
+forward(200)
+right(90)
+forward(100)
+right(90)
+forward(200)
+right(90)
+forward(100)
+
+exitonclick()
