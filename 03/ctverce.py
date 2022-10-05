31584b599f78ac4c2a1e86178dc78870e8af0e56
Pavučina <pavucina.p@gmail.com>
2022-10-05 21:52:55 +0200

    Pyladies - lesson 3.

--- 03/ctverce.py
+++ 03/ctverce.py
@@ -5,7 +5,7 @@ shape("turtle")
 
 for _ in range(18):     # 18 opakování cyklu pod; _ nahrazuje proměnnou, když se nemusí specifikovat
     left(20)            # otočení o 20
-    for _ in range(4):  # opakování pohybu pod for
+    for _ in range(4):  # opakování pohybu pod prvním for
         forward(200)
         right(90)
    
