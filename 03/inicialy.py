c56d4702522781c84a998f30c25e82b668023855
Pavučina <pavucina.p@gmail.com>
2022-10-05 21:55:06 +0200

    Pyladies - lesson 3.

--- 03/inicialy.py
+++
@@ -1,4 +0,0 @@
-jmeno = input("Jaké je tvé jméno: ")
-prijmeni = input("Jaké je tvé příjmění: ")
-
-print(jmeno[0].upper(), ".", prijmeni[0].upper(), ".")
--- 03/while_break.py
+++
@@ -1,7 +0,0 @@
-from random import randrange
-
-while True:
-    if 13 == randrange(100):
-        print("Super, to by šlo!")
-        break                           # ukončí cyklus
-    print("Ne!")
