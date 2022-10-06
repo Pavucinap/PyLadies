f469d6089fdadf7b70b6bfddd6509b7c36b12e6a
Pavučina <pavucina.p@gmail.com>
2022-10-06 23:04:07 +0200

    Pyladies lesson 03, homework.

--- ulohy_03/vlocky.py
+++ ulohy_03/vlocky.py
@@ -1,43 +1,46 @@
-from sys import builtin_module_names
-from turtle import back, forward, right, left, backward
-from turtle import penup, pendown
-from turtle import shape, exitonclick
-from turtle import stamp, color
+from turtle import back, forward, right, left, backward, goto, penup, pendown, shape, exitonclick, stamp, color, pensize
+from random import randrange
 
-shape("turtle")
-
-penup()
-backward(400)
-pendown()
-color("blue")
-
-backward(20)
-right(120)
-backward(20)
-forward(20)
-right(120)
-backward(20)
-forward(20)
-
-right(120)
-backward(25)
-left(120)
-
-for _ in range(5):
-    backward(45)
-    forward(20)
-    right(120)
-    forward(20)
-    backward(20)
-    right(120)
-    forward(20)
-    backward(20)
-
-    right(120)
-    forward(25)
-    right(60)
+shape("circle")
 
 
 
+for _ in range(randrange(5,15)):
+    penup()
+    left(randrange(0, 360))
+    goto(randrange(-250, 250),randrange(-250, 250))
+    pendown()
+    
+    n = randrange(1, 5)
+        
+    if n > 3:
+        color("blue")
+        pensize(5)
+    elif n > 1:
+        color("dodgerblue1")
+        pensize(3)
+    else:
+        color("cadetblue2")
+        pensize(1)
+
+    for _ in range(6):
+        backward(n * 30)
+        forward(n * 10)
+        for _ in range(2):
+            right(120)
+            forward(n * 4)
+            backward(n * 4)
+        
+        right(120)
+        forward(n * 5)
+        for _ in range(2):
+            right(120)
+            forward(n * 10)
+            backward(n * 10)
+        right(120)
+        forward(n * 15)
+        right(60)
+
+    stamp()
 
 exitonclick()
