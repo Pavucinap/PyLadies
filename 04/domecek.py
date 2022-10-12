1147e5c9cd9ebd93460155e369fc0de72fa682e6
Pavučina <pavucina.p@gmail.com>
2022-10-12 18:46:06 +0200

    Domeček

---
+++ 04/domecek.py
@@ -0,0 +1,17 @@
+from turtle import fillcolor, begin_fill, forward, left, end_fill, exitonclick
+
+size = 100
+color = "#FFE66D"
+
+#Nastavení barvy
+fillcolor(color)
+begin_fill()
+
+# Nakreslí čtverec
+for _ in range(4):
+    forward(size)
+    left(90)
+
+end_fill()
+
+exitonclick()
