79ae86f30c1a99d8df1b8c773df8a4684338ba96
Pavučina <pavucina.p@gmail.com>
2022-10-15 21:40:25 +0200

    Doplněné konstanty, oprava ukončení.

--- ulohy_04/Caesar_cipher.py
+++ ulohy_04/Caesar_cipher.py
@@ -3,25 +3,26 @@
 while True:
     text = input("Zadej text, který chceš zakódovat: ")
     if text == "":
-        break
+        exit()
     elif text.isascii():
         while True:
             key = input("Jaký klíč chceš použít? ")
             if key.isdigit() and int(key) > 0 and int(key) % 26 != 0:
                 break
-            print("Klíč musí být celé kladné číslo.",
+            print("Klíč musí být celé kladné číslo. \n"
                 "Násobek 26 nic nezašifruje.")
-        
-        # Šifra - velká písmena 65 - 90, malá písmena 97 - 122 (26 písmen)
-        new_text = ""
-        for char in text:
-            if char.isupper():
-                new_text += chr((ord(char) + int(key) - 65) % 26 + 65)
-            elif char.islower():
-                new_text += chr((ord(char) + int(key) - 97) % 26 + 97)
-            else:
-                new_text += chr(ord(char))
-        print(new_text)
-        
         break
     print("Bohužel text obsahuje nepovolené znaky, které nejsou ASCII.")
+
+# Šifra - velká písmena 65 - 90, malá písmena 97 - 122 (26 písmen)
+a = ord("a")
+A = ord("A")
+new_text = ""
+for char in text:
+    if char.isupper():
+        new_text += chr((ord(char) + int(key) - A) % 26 + A)
+    elif char.islower():
+        new_text += chr((ord(char) + int(key) - a) % 26 + a)
+    else:
+        new_text += chr(ord(char))
+print(new_text)
