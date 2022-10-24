63aa2b90efaaa4945abda8506960a1dc27cecacc
Pavučina <pavucina.p@gmail.com>
2022-10-24 19:53:27 +0200

    Oprava proměnné na řádku 19.

--- ulohy_05/povinna_uloha_6_zviratka.py
+++ ulohy_05/povinna_uloha_6_zviratka.py
@@ -1,4 +1,4 @@
-zvirata = ["pes", "kočka", "králík","had"]
+zvirata = ["pes", "kočka", "králík", "had"]
 zvirata.append("andulka")
 
 # Slovník ze seznamu, klíč je druhý znak ve slově a hodnota je slovo
@@ -16,7 +16,7 @@ for key in sort_letters:
     sort_value[key] = zvirata_dict[key]
 
 # Vypsání pouze hodnot slovníku jako seznam
-seznam_zvirat = list(zvirata_dict.values())
+seznam_zvirat = list(sort_value.values())
 print(seznam_zvirat)
 
 # Než jsem si přečetla pořádně zadání, že to má být přes slovníky, tak jsem 
