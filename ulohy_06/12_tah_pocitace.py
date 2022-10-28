1d70400f8046314f3ae50d39436e6a7ef535b055
Pavučina <pavucina.p@gmail.com>
2022-10-28 18:03:24 +0200

    Přidány soubory po vyřešení konfliktu

---
+++ ulohy_06/12_tah_pocitace.py
@@ -0,0 +1,27 @@
+# Napiš funkci tah_pocitace, která dostane řetězec s herním polem, 
+# vybere pozici, na kterou hrát, a vrátí herní pole se zaznamenaným 
+# tahem počítače. Použij jednoduchou náhodnou „strategii”:
+    # 1. Vyber číslo od 0 do 19.
+    # 2. Pokud je dané políčko volné, hrej na něj.
+    # 3. Pokud ne, opakuj od bodu 1.
+# Hlavička funkce by tedy měla vypadat nějak takhle:
+    # def tah_pocitace(pole):
+    # "Vrátí herní pole se zaznamenaným tahem počítače"
+
+from random import randrange
+
+hraci_pole = 20 * "-"
+
+def tah_pocitace(pole):
+    """Vrátí herní pole se zaznamenaným tahem počítače"""
+    while True:
+        pozice = randrange(19)
+        if pole[pozice] != "-":
+            continue
+        break
+    symbol = "o"
+    vysledek = pole[:pozice] + symbol + pole[pozice + 1:]
+   
+    return vysledek
+
+print(tah_pocitace(hraci_pole))
