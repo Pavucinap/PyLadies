36b88fd36b9c0248a83da3a8aee427f228efa595
Pavučina <pavucina.p@gmail.com>
2022-11-06 19:41:49 +0100

    Vytvoření 4 modulů.

---
+++ ulohy_07/1D_piskvorky/ai.py
@@ -0,0 +1,15 @@
+from random import randrange
+
+def tah_pocitace(pole, symbol):
+    """Vrátí herní pole se zaznamenaným tahem počítače"""
+    while True:
+        # Náhodně vybere pozici od 0 do 19
+        pozice = randrange(19)
+        # Ošetření, že nemůže zadat na obsazenou pozici
+        if pole[pozice] == "-":
+            break
+
+    vysledek_pocitac = pole[:pozice] + symbol + pole[pozice + 1:]
+   
+    return vysledek_pocitac
+    
