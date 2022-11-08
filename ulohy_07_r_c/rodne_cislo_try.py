7146f05e744fb8966c5a176320b8637769134e1b
Pavučina <pavucina.p@gmail.com>
2022-11-08 23:23:06 +0100

    Opraveno formátování dle PEP8.

--- ulohy_07_r_c/rodne_cislo_try.py
+++ ulohy_07_r_c/rodne_cislo_try.py
@@ -1,14 +1,16 @@
 def format(rodne_cislo):
     """Ověří správnost zápisu RRMMDD/XXXX"""
-    if not (rodne_cislo[0:6].isdigit() and rodne_cislo[7:11].isdigit() 
+    if not (rodne_cislo[0:6].isdigit() and rodne_cislo[7:11].isdigit()
             and len(rodne_cislo) == 11 and rodne_cislo[6] == "/"):
         raise ValueError("Správný zápis je ve formátu RRMMDD/XXXX")
+
 
 def delitelnost(rodne_cislo):
     """Ověří, zda je číslo dělitelné 11"""
     cislo = rodne_cislo[0:6] + rodne_cislo[7:11]
     if not int(cislo) % 11 == 0:
         raise ValueError("Rodné číslo musí být dělitelné 11.")
+
 
 def datum(rodne_cislo):
     """Zjistí, zda jsou čísla správně a vypíše datum narození."""
@@ -18,18 +20,19 @@ def datum(rodne_cislo):
 
     if 22 < rok < 85:
         raise ValueError("Rok se může pohybovat v rozmezí 1985 - 2022.")
-    
-    if 0 <= rok <= 22:
-        rok = (f"20{rok:02d}") 
-    else:
-        rok = (f"19{rok:02d}") 
 
-    if 50 < mes <=62:
+    if 0 <= rok <= 22:
+        rok = (f"20{rok:02d}")
+    else:
+        rok = (f"19{rok:02d}")
+
+    if 50 < mes <= 62:
         mes = mes - 50
 
     if mes == 00 or mes > 12:
-        raise ValueError("Měsíce se uvádějí ve formátu 01-12 pro muže a 51-62 pro ženy.")
-           
+        raise ValueError("Měsíce se uvádějí ve formátu 01-12 pro muže \
+            a 51-62 pro ženy.")
+
     # Ošetření počtu dnů podle měsíců
     dlouhe = [1, 3, 5, 7, 8, 10, 12]
     kratke = [4, 6, 9, 11]
@@ -48,15 +51,17 @@ def datum(rodne_cislo):
 
     return datum_nar
 
+
 def pohlavi(rodne_cislo):
     """Zjistí pohlaví zadavatele"""
     if 0 < int(rodne_cislo[2:4]) <= 12:
-       odpoved = "muž"
+        odpoved = "muž"
     else:
         odpoved = "žena"
-    
+
     return odpoved
- 
+
+
 while True:
     r_c = input("Jaké je tvé rodné číslo? ")
 
@@ -68,5 +73,5 @@ while True:
     except ValueError as e:
         print(e)
         continue
-       
+
     break
